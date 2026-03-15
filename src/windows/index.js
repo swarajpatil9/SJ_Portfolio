629d3f9efd5369ddbf892d117e496efd27ab538e
swarajpatil232 <swarajpatil2823@gmail.com>
2026-03-15 23:37:06 +0530

    Finder-2

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,6 +2,7 @@ import TerminalWindow from "./Terminal";
 import Safari from "./Safari";
 import BlogArticle from "./BlogArticle";
 import ResumeWindow from "./Resume";
+import Finder from "./Finder";
 
-export { TerminalWindow, BlogArticle, ResumeWindow };
+export { TerminalWindow, BlogArticle, ResumeWindow, Finder };
 export default TerminalWindow;
